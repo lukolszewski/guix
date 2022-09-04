969e5e3aee9c5d3a07e97a5f2abb40a4885e6dff
Lukasz Olszewski <dev@lukaszolszewski.info>
2022-09-04 23:06:04 +0200

    improve code style

--- gnu/packages/libldm.scm
+++ gnu/packages/libldm.scm
@@ -1,7 +1,8 @@
 (define-module (gnu packages libldm)
   #:use-module (gnu packages)
   #:use-module (guix packages)
-  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module ((guix licenses)
+                #:prefix license:)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages base)
   #:use-module (gnu packages autotools)
@@ -18,44 +19,52 @@
   #:use-module (guix git-download))
 
 (define-public libldm
-(package
- (name "libldm")
- (version "0.2.5")
- (source (origin
-          (method git-fetch)
-          (uri (git-reference
-                (url "https://github.com/mdbooth/libldm.git")
-                (commit (string-append "libldm-" version))))
-          (file-name (git-file-name name version))
-          (sha256
-           (base32
-            "08iz3kq4ci79abpyxwwqmzi3bayyk4s29n8h1jqgdgk5yskwgnrn"))))
- (build-system gnu-build-system)
- (inputs (list json-glib glib zlib readline lvm2 libgudev))
- (native-inputs (list which
-                      m4
-                      libtool
-                      autoconf-wrapper
-                      automake
-                      pkg-config
-                      `(,glib "bin")
-                      gtk-doc
-                      libxml2
-                      libxslt
-                      docbook-xsl))
- (arguments
-  '(#:tests? #f
-    #:parallel-build? #t
-    #:phases (modify-phases %standard-phases
-                            (add-before 'configure 'set-env
-                                        (lambda _
-                                          (setenv "CONFIG_SHELL" (which ""))#t))
-                            (add-before 'bootstrap 'run-gtkdocize
-                                        (lambda _
-                                          (invoke "gtkdocize")))
-                            (replace 'bootstrap (lambda _ (invoke "autoreconf" "-fiv"))))))
- (home-page "https://github.com/mdbooth/libldm")
- (synopsis "Manager for Microsoft Windows dynamic disks.")
- (description
-  "Libldm is a library for managing Microsoft Windows dynamic disks, which use Microsoft's LDM metadata.  It can inspect them, and also create and remove device-mapper block devices which can be mounted.")
- (license license:gpl3)) )
+  (package
+    (name "libldm")
+    (version "0.2.5")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/mdbooth/libldm.git")
+                    (commit (string-append "libldm-" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "08iz3kq4ci79abpyxwwqmzi3bayyk4s29n8h1jqgdgk5yskwgnrn"))))
+    (build-system gnu-build-system)
+    (inputs (list json-glib
+                  glib
+                  zlib
+                  readline
+                  lvm2
+                  libgudev))
+    (native-inputs (list which
+                         m4
+                         libtool
+                         autoconf-wrapper
+                         automake
+                         pkg-config
+                         `(,glib "bin")
+                         gtk-doc
+                         libxml2
+                         libxslt
+                         docbook-xsl))
+    (arguments
+     '(#:tests? #f
+       #:parallel-build? #t
+       #:phases (modify-phases %standard-phases
+                  (add-before 'configure 'set-env
+                    (lambda _
+                      (setenv "CONFIG_SHELL"
+                              (which "")) #t))
+                  (add-before 'bootstrap 'run-gtkdocize
+                    (lambda _
+                      (invoke "gtkdocize")))
+                  (replace 'bootstrap
+                    (lambda _
+                      (invoke "autoreconf" "-fiv"))))))
+    (home-page "https://github.com/mdbooth/libldm")
+    (synopsis "Manager for Microsoft Windows dynamic disks")
+    (description
+     "Libldm is a library for managing Microsoft Windows dynamic disks, which use Microsoft's LDM metadata.  It can inspect them, and also create and remove device-mapper block devices which can be mounted.")
+    (license license:gpl3)))
--- gnu/services/libldm.scm
+++ gnu/services/libldm.scm
@@ -12,39 +12,36 @@
   #:use-module (gnu services base)
   #:use-module (gnu services shepherd)
   #:use-module (gnu packages libldm)
-  #:export     (libldm-configuration
-                libldm-configuration?
-                libldm-service-type))
-
+  #:export (libldm-configuration libldm-configuration? libldm-service-type))
 
 (define-record-type* <libldm-configuration>
-  libldm-configuration
-  make-libldm-configuration
-  libldm-configuration?
-  (package        libldm-configuration-package
-                  (default libldm))
-  (action         libldm-configuration-action
-                  (default '("create" "all"))))
+                     libldm-configuration
+                     make-libldm-configuration
+                     libldm-configuration?
+                     (package
+                       libldm-configuration-package
+                       (default libldm))
+                     (action libldm-configuration-action
+                             (default '("create" "all"))))
 
 (define (libldm-shepherd-service config)
   "Return a <shepherd-service> for libldm with CONFIG"
-  (let* ((libldm     (libldm-configuration-package config))
-         (action     (libldm-configuration-action config)))
-    (list (shepherd-service
-      (documentation "Run ldmtool to create Windows dynamic disc device nodes at startup.")
-      (provision '(libldmd))
-      (one-shot? #t)
-      (start #~(make-forkexec-constructor
-                (append
-                 (list (string-append #$libldm "/bin/ldmtool"))
-                 '(#$@action))))
-      (stop #~(make-kill-destructor))))))
+  (let* ((libldm (libldm-configuration-package config))
+         (action (libldm-configuration-action config)))
+    (list (shepherd-service (documentation
+                             "Run ldmtool to create Windows dynamic disc device nodes at startup.")
+                            (provision '(libldmd))
+                            (one-shot? #t)
+                            (start #~(make-forkexec-constructor (append (list (string-append #$libldm
+                                                                               "/bin/ldmtool"))
+                                                                        '(#$@action))))
+                            (stop #~(make-kill-destructor))))))
 
 (define libldm-service-type
- (service-type
-   (name 'libldm)
-   (extensions
-    (list (service-extension shepherd-root-service-type libldm-shepherd-service)))
-   (default-value (libldm-configuration))
-   (description
-    "Run ldmtool to create device nodes for Windows dynamic discs so they can be mounted")) )
+  (service-type (name 'libldm)
+                (extensions (list (service-extension
+                                   shepherd-root-service-type
+                                   libldm-shepherd-service)))
+                (default-value (libldm-configuration))
+                (description
+                 "Run ldmtool to create device nodes for Windows dynamic discs so they can be mounted")))
